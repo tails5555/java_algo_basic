34f291295dbc8ef24efd7eb7b660c102db4273b8
tails5555 <tails5555@naver.com>
2019-06-02 13:05:26 +0900

    Init Topological Sort + DFS Algorithm Source 위상 정렬 + DFS 알고리즘 기본 소스 추가 완료. 다익스트라, 플로이드 워샬 등은 다음에 하겠습니다.

--- algorithm/src/net/kang/graph/topological/Topological_DFS.java
+++ algorithm/src/net/kang/graph/topological/Topological_DFS.java
@@ -2,19 +2,35 @@ package net.kang.graph.topological;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Queue;
 
-// 위상 정렬(Topological Sort) BFS 방법.
-// 1 -> 3 -> 4 -> 6 -> 2 -> 5 -> 7 순으로 나옵니다.
-public class Topological_BFS {
+// 위상 정렬(Topological Sort) DFS 방법.
+// 1 -> 3 -> 6 -> 2 -> 5 -> 7 -> 4 순으로 나옵니다.
+// 위상 별 순서만 보장되면 됩니다. 순서에는 정답이 따로 없습니다.
+// 다만 DFS 로 처리할 때, 차수가 0 개인 정점을 중복 탐색하는 경우가 있어 visited 배열로 처리해야 합니다. 그래서 BFS 를 사용하는 것이 나을 수도...
+public class Topological_DFS {
     static final int N = 7;
 
     static List<Integer>[] graph;
     static int[] degree;
+    static boolean[] visited;
+    static StringBuilder sb;
 
-    static void topological_bfs(){
+    static void dfs(int vtx){
+        if(visited[vtx]) return;
+        if(degree[vtx] == 0) {
+            visited[vtx] = true;
+            sb.append(String.format("%d ", vtx));
+            for (int next : graph[vtx]) {
+                if (degree[next] > 0) {
+                    degree[next] -= 1;
+                    dfs(next);
+                }
+            }
+        }
+    }
+
+    static void topological(){
         degree = new int[N + 1];
         for(int k = 1; k <= 7; k++){
             for(int v : graph[k]){
@@ -22,21 +38,10 @@ public class Topological_BFS {
             }
         }
 
-        Queue<Integer> queue = new LinkedList<>();
+        sb = new StringBuilder();
+        visited = new boolean[N + 1];
         for(int v = 1; v <= N; v++){
-            if(degree[v] == 0)
-                queue.offer(v);
-        }
-
-        StringBuilder sb = new StringBuilder();
-        while(!queue.isEmpty()){
-            int cur = queue.poll();
-            sb.append(String.format("%d ", cur));
-            for(int v : graph[cur]){
-                degree[v] -= 1;
-                if(degree[v] == 0)
-                    queue.offer(v);
-            }
+            if(degree[v] == 0) dfs(v);
         }
 
         System.out.println(sb.toString());
@@ -58,6 +63,6 @@ public class Topological_BFS {
 
     public static void main(String[] args){
         graph_initialize();
-        topological_bfs();
+        topological();
     }
 }
